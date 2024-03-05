899a6e9c31a3bea4afc34509d53d2fc943316f05
Husein Naimov <huseinnaimov72@gmail.com>
2024-03-05 16:17:18 +0500

    update

--- src/adapters/cvat_sdk_adapter.py
+++ src/adapters/cvat_sdk_adapter.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from cvat_sdk.api_client import Configuration, ApiClient
+from cvat_sdk.core.helpers import get_paginated_collection
 
 
 class CVATSdkFacadeInterface(ABC):
@@ -9,21 +10,43 @@ class CVATSdkFacadeInterface(ABC):
 
 
 class CVATSdkFacade(CVATSdkFacadeInterface):
-
-    
-    configuration = Configuration(host="https://app.cvat.ai/", username='huseinnaimov@bk.ru',
-                                  password='Husia2000',)
+    configuration = Configuration(host="http://192.168.13.20:8080/", username='hnaimov',
+                                  password='Lr3&qQvA',)
 
     def get_statistics_by_assignee(self, assignee: str):
         bbox_total = 0
         assignee_jobs_statistics = []
         job_id_to_stage = {}
+        assignee_projects_id = []
+        assignee_tasks_id = []
         with ApiClient(self.configuration) as client:
-            all_jobs = client.jobs_api.list()[0]["results"]
-            for jobs in all_jobs:
-                if jobs["assignee"]:
-                    if jobs["assignee"]["username"] == assignee:
-                        job_id_to_stage[jobs["id"]] = jobs["stage"]
+            all_jobs = get_paginated_collection(
+                client.jobs_api.list_endpoint)
+            all_tasks = get_paginated_collection(
+                client.tasks_api.list_endpoint)
+            all_projects = get_paginated_collection(
+                client.projects_api.list_endpoint)
+
+            for project in all_projects:
+                if project["assignee"]:
+
+                    if project["assignee"]["username"] == assignee:
+                        assignee_projects_id.append(project["id"])
+
+            for task in all_tasks:
+                if task["assignee"]:
+                    if task["assignee"]["username"] == assignee:
+                        assignee_tasks_id.append(task["id"])
+            for job in all_jobs:
+                if job["assignee"]:
+                    if job["assignee"]["username"] == assignee:
+                        job_id_to_stage[job["id"]] = job["stage"]
+                elif job["project_id"] in assignee_projects_id:
+                    job_id_to_stage[job["id"]] = job["stage"]
+
+                elif job["task_id"] in assignee_tasks_id:
+                    job_id_to_stage[job["id"]] = job["stage"]
+
             for job_id, job_stage in job_id_to_stage.items():
                 jobs_annotations = client.jobs_api.retrieve_annotations(job_id)
                 jobs_annotations = jobs_annotations[0]["shapes"]
@@ -31,5 +54,4 @@ class CVATSdkFacade(CVATSdkFacadeInterface):
                 bbox_total += bbox_count
                 assignee_jobs_statistics.append(
                     {"job_id": job_id, "stage": job_stage, "bbox_count": bbox_count})
-
         return {"assignee": assignee, "bbox_total": bbox_total, "jobs": assignee_jobs_statistics}
